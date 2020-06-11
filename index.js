ae8fca96c2c4d1507f409215dd81abd504cec983
mr-fool <mr-fool@users.noreply.github.com>
2020-06-11 13:23:33 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+new Vue( {
+   el: '#app',
+   data: {
+      //initialize our list of 'ingredients'
+      textInput: ''
+   },
+   computed: { //Turn data into viewable values
+      identicon: function() {
+         return jdenticon.toSvg(this.textInput, 200);
+      },
+      reverse: function() {
+         return this.textInput.split('').reverse().join('')
+      }
+   },
+   methods: { // Use these function to change data 
+      onInput: function(event) {
+        this.textInput = event.target.value;
+      }
+   },
+   template: `
+   <div>
+      <h3>My Identicon Generator</h3>
+
+      <div>
+         Input: 
+         <input v-on:input="onInput"/>
+      </div>
+
+      <div>
+         Output: 
+         {{<div v-html="identicon" />}}
+      </div>
+   <div>
+   `
+})
+https://codepen.io/sgrider/pen/eMqjgw?editors=1011
